59033c8fc838c3273b76c3a317fd0d888a759a21
LegrainAnthony <legrain.anthony51@gmail.com>
2024-03-27 19:15:16 +0100

    :wrench: Architecture & files configuration

---
+++ eslintrc.ts
@@ -0,0 +1,33 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'plugin:vue/vue3-strongly-recommended',
+      'eslint:recommended',
+    ],
+    parserOptions: {
+      parser: '@babel/eslint-parser',
+      requireConfigFile: false,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+    overrides: [
+      {
+        files: [
+          '**/__tests__/*.{j,t}s?(x)',
+          '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        ],
+        env: {
+          jest: true,
+        },
+      },
+    ],
+  };
