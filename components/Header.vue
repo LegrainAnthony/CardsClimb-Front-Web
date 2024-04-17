bb25066ffa2f8cefb1324181ee78b573ffe573c1
BenjaminAdrien <benadrien314@gmail.com>
2024-04-17 17:28:01 +0200

    :construction: dev du layout

---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+
+  const open = ref(false)
+</script>
+
+<template>
+  <header>
+    <UContainer class="flex justify-between">
+      <div>
+      </div>
+      <ul class="hidden lg:flex justify-around gap-x-8">
+        <li>Cards</li>
+        <li>Blitz</li>
+        <li>Profile</li>
+      </ul>
+      <div class="flex items-center justify-center lg:flex gap-1.5">
+        <UButton color="white">Sign in</UButton>
+        <UButton
+          class="hidden lg:flex"
+          color="black"
+        >Sign up</UButton>
+        <UButton
+          v-model="open"
+          class="lg:hidden"
+          size="md"
+          variant="ghost"
+          color="gray"
+          :ui="{
+            rounded: 'rounded-full'
+          }"
+          :icon="open ? 'i-heroicons-bars-3-20-solid' : 'i-heroicons-x-mark-20-solid'"
+          @click="open = !open"
+        />
+      </div>
+    </UContainer>
+  </header>
+</template>
+
+<style></style>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Header />
+
+    <UContainer>
+      <slot />
+    </UContainer>
+
+  </div>
+</template>
