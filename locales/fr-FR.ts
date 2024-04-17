f936b0b439cf1e9835e818d2f35ee03aeff52be2
BenjaminAdrien <benadrien314@gmail.com>
2024-04-17 12:10:04 +0200

    :construction: initialisation NuxtUI et ajout Example Form

---
+++ locales/fr-FR.ts
@@ -0,0 +1,7 @@
+export default defineI18nLocale(async (locale) => {
+  return {
+    welcome: 'Bienvenue',
+    invalidEmail: 'Email non valide',
+    required: 'Requis',
+  };
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: {
+    enabled: true,
+  },
+  modules: ['@nuxtjs/i18n', '@vueuse/nuxt', '@vee-validate/nuxt', '@nuxt/ui'],
+  i18n: {
+    locales: [
+      {
+        code: 'fr',
+        name: 'Français (France)',
+        file: 'fr-FR.ts',
+      },
+    ],
+    lazy: true,
+    langDir: 'locales',
+    defaultLocale: 'fr',
+  },
+  // veeValidate: {
+  //   // disable or enable auto imports
+  //   autoImports: true,
+  // },
+});
