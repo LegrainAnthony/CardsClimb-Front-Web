950637b557dab5d070046000df87ce1c93064685
BenjaminAdrien <benadrien314@gmail.com>
2024-04-18 12:14:55 +0200

    :construction: creation du layout

--- components/Header.vue
+++ components/Header.vue
@@ -1,26 +1,106 @@
 <script lang="ts" setup>
+  defineProps({
+    propsOpen: {
+      type: Boolean,
+      required: true,
+    },
+    links: {
+      type: Array,
+      required: true
+    }
+  })
+  const emit = defineEmits(['update:menu'])
+  const hnav = ref(true)
 
-  const open = ref(false)
+  const colorMode = useColorMode()
+
+  const switchColorMode = () => (colorMode.preference = (colorMode.value === 'light' ? 'dark' : 'light'))
+
 </script>
 
 <template>
-  <header>
-    <UContainer class="flex justify-between">
-      <div>
-      </div>
-      <ul class="hidden lg:flex justify-around gap-x-8">
-        <li>Cards</li>
-        <li>Blitz</li>
-        <li>Profile</li>
+  <header class="bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50">
+    <UContainer class="flex items-center justify-between gap-3 h-16">
+      <NuxtLink
+        to="/"
+        class="lg:flex-1 flex items-center gap-1.5"
+      >
+        <UIcon
+          class="w-7 h-7 lg:w-10 lg:h-10 flex-shrink-0 text-primary"
+          name="i-heroicons-puzzle-piece-solid"
+        />
+        <span class="font-bold text-xl lg:text-2xl">
+          Card<span class="text-primary">Climb</span>
+        </span>
+      </NuxtLink>
+      <ul
+        v-if="!hnav"
+        class="hidden lg:flex justify-around gap-x-8"
+      >
+        <li class="relative">
+          <NuxtLink
+            class="text-sm/6 font-semibold flex items-center gap-1 hover:text-primary"
+            to="/cards"
+          >
+            Cards
+          </NuxtLink>
+        </li>
+        <NuxtLink
+          class="text-sm/6 font-semibold flex items-center gap-1 hover:text-primary"
+          to="/blitz"
+        >
+          Blitz
+        </NuxtLink>
+        <NuxtLink
+          class="text-sm/6 font-semibold flex items-center gap-1 hover:text-primary"
+          to="/profile"
+        >
+          Profile
+        </NuxtLink>
       </ul>
+      <UHorizontalNavigation
+        v-else
+        class="hidden lg:flex justify-around gap-x-8"
+        :links="links"
+      />
       <div class="flex items-center justify-center lg:flex gap-1.5">
-        <UButton color="white">Sign in</UButton>
+        <UButton
+          size="md"
+          variant="ghost"
+          color="gray"
+          :ui="{
+            rounded: 'rounded-full'
+          }"
+          @click="switchColorMode()"
+        >
+          <ColorScheme placeholder="">
+            <Icon
+              v-if="colorMode.value === 'dark'"
+              name="i-heroicons-moon-20-solid"
+              class="text-xl"
+            />
+            <Icon
+              v-else
+              name="i-heroicons-sun-20-solid"
+              class="text-xl"
+            />
+          </ColorScheme>
+        </UButton>
+        <UButton
+          color="white"
+          to="/login"
+          @click="emit('update:menu', false)"
+        >
+          Sign in
+        </UButton>
         <UButton
           class="hidden lg:flex"
           color="black"
-        >Sign up</UButton>
+          to="/signup"
+        >
+          Sign up
+        </UButton>
         <UButton
-          v-model="open"
           class="lg:hidden"
           size="md"
           variant="ghost"
@@ -28,8 +108,8 @@
           :ui="{
             rounded: 'rounded-full'
           }"
-          :icon="open ? 'i-heroicons-bars-3-20-solid' : 'i-heroicons-x-mark-20-solid'"
-          @click="open = !open"
+          :icon="!propsOpen ? 'i-heroicons-bars-3-20-solid' : 'i-heroicons-x-mark-20-solid'"
+          @click="emit('update:menu', !propsOpen)"
         />
       </div>
     </UContainer>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,59 @@
+<script lang="ts" setup>
+  const open = ref(false)
+
+  // pour fermer le menu au click
+  const click = () => {
+    open.value = false
+  }
+  const links = [
+    {
+      label: 'Cards',
+      icon: 'i-heroicons-inbox-stack',
+      to: '/cards',
+      click
+    },
+    {
+      label: 'Blitz',
+      icon: 'i-heroicons-bolt-solid',
+      to: '/blitz',
+      click
+    },
+    {
+      label: 'Profile',
+      icon: 'i-heroicons-user',
+      to: '/profile',
+      click
+    }
+  ]
+</script>
 <template>
   <div>
-    <Header />
+    <Header
+      :props-open="open"
+      :links=links
+      @update:menu="open = $event"
+    />
 
     <UContainer>
-      <slot />
+      <div
+        v-if=open
+        class="px-4 sm:px-6 pt-3 lg:hidden"
+      >
+        <UVerticalNavigation
+          class="pb-6"
+          :links="links"
+        />
+        <UDivider class="mb-4" />
+        <UButton
+          block
+          to="/signup"
+          @click="click()"
+        >Sign Up</UButton>
+      </div>
+      <div :class="open ? 'hidden lg:block ' : 'lg:block'">
+        <slot />
+      </div>
+
     </UContainer>
 
   </div>
