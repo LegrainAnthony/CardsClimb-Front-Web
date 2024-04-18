7d821827a0ae3ca0340e0df184139a57ef922a13
BenjaminAdrien <benadrien314@gmail.com>
2024-04-18 17:19:59 +0200

    :construction: main page

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,24 @@
 </script>
 
 <template>
-  <UCard class="mt-10">
-    <template #header>
-      <div class="flex justify-between">
-        <h1>Welcome to Nuxt UI Starter</h1>
-        <ColorScheme>
-          <USelect
-            v-model="$colorMode.preference"
-            :options="['system', 'light', 'dark']"
-          />
-        </ColorScheme>
+  <UContainer class="min-h-[calc(100vh-4rem)] flex flex-col justify-center gap-y-10">
+    <div class="-mt-20 flex flex-col justify-center items-center gap-y-2">
+      <h1 class="text-center font-bold text-2xl">Welcome to <span class="text-primary">CardClimb</span></h1>
+      <p class="text-center text-slate-400">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim magnam esse explicabo
+      </p>
+    </div>
+    <div class="flex justify-around">
+      <UIcon
+        class="w-48 h-48"
+        name="i-heroicons-document-arrow-down"
+      />
+      <div class="flex items-center">
+        <UButton
+          to="/blitz"
+          icon="i-heroicons-bolt-solid"
+        >Blitz Mode</UButton>
       </div>
-    </template>
-    <UButton
-      icon="i-heroicons-book-open"
-      to="https://ui.nuxt.com"
-      target="_blank"
-    >Open Nuxt UI Documentation</UButton>
-    <ExamplesForm class="mt-10" />
-  </UCard>
+    </div>
+  </UContainer>
 </template>
