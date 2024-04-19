a80e510cc62fad7ba99090776e1cdbd4847f909c
BenjaminAdrien <benadrien314@gmail.com>
2024-04-19 17:07:14 +0200

    :construction: modif css et ajout btn pour isConnected (test purpose)

--- pages/index.vue
+++ pages/index.vue
@@ -5,32 +5,34 @@
 </script>
 
 <template>
-  <UContainer class="min-h-[calc(100vh-4rem)] flex flex-col justify-center gap-y-10">
-    <div class="-mt-20 flex flex-col justify-center items-center gap-y-2">
-      <h1 class="text-center font-bold text-2xl">
-        {{ isConnected ? t('WelcomeBack') : t('welcomeTo') }}
+  <UContainer class="min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center gap-y-10">
+    <div class="-mt-20 flex flex-col justify-center items-center gap-y-6">
+      <h1 class="text-center font-bold text-4xl sm:text-5xl">
+        {{ isConnected ? t('welcomeBack') : t('welcomeTo') }}
         <span class="text-primary">
           {{ isConnected ? 'UserName' : 'CardClimb' }}
         </span>
       </h1>
       <p
         v-if=isConnected
-        class="text-center text-gray-500 dark:text-gray-400"
+        class="text-lg text-center text-gray-500 dark:text-gray-400"
         v-html="t('reviewMessage', { number: 7 })"
       >
       </p>
       <p
         v-else
-        class="text-center text-gray-500 dark:text-gray-400"
+        class="text-lg text-center text-gray-500 dark:text-gray-400"
       >
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim magnam esse explicabo
       </p>
     </div>
     <div class="flex items-center justify-center gap-x-6 lg:gap-x-16">
-      <UIcon
-        class="w-36 h-36 lg:w-48 lg:h-48 shrink"
-        name="i-heroicons-document-arrow-down"
-      />
+      <NuxtLink to="/cards">
+        <UIcon
+          class="w-36 h-36 lg:w-48 lg:h-48 shrink"
+          name="i-heroicons-document-arrow-down"
+        />
+      </NuxtLink>
       <div class="flex flex-col items-center gap-y-4">
         <UButton
           v-if="isConnected"
@@ -50,6 +52,12 @@
           icon="i-heroicons-bolt-20-solid"
         >{{ t('blitz') }}</UButton>
       </div>
+    </div>
+    <div class="flex justify-center items-center absolute bottom-20">
+      <UButton
+        variant="outline"
+        @click="isConnected = !isConnected"
+      >Change isConnected</UButton>
     </div>
   </UContainer>
 </template>
