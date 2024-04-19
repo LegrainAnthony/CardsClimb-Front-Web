b7e12fb0ae6aa410d5676a3839e193635fb3ddba
BenjaminAdrien <benadrien314@gmail.com>
2024-04-19 11:37:38 +0200

    :construction: ajout affichage utilisateur connecté

--- locales/fr-FR.ts
+++ locales/fr-FR.ts
@@ -1,7 +1,13 @@
 export default defineI18nLocale(async (locale) => {
   return {
-    welcome: 'Bienvenue',
+    welcomeTo: 'Bienvenue dans',
+    welcomeBack: 'Bon retour',
+    reviewMessage:
+      'Vous avez <span class="font-bold text-primary">{number}</span> cartes à réviser aujourd\'hui !',
     invalidEmail: 'Email non valide',
     required: 'Requis',
+    review: 'Réviser',
+    profile: 'Profile',
+    blitz: 'Blitz',
   };
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,54 @@
 <script setup lang="ts">
+  const isConnected = ref(false)
+
+  const { t } = useI18n()
 </script>
 
 <template>
   <UContainer class="min-h-[calc(100vh-4rem)] flex flex-col justify-center gap-y-10">
     <div class="-mt-20 flex flex-col justify-center items-center gap-y-2">
-      <h1 class="text-center font-bold text-2xl">Welcome to <span class="text-primary">CardClimb</span></h1>
-      <p class="text-center text-slate-400">
+      <h1 class="text-center font-bold text-2xl">
+        {{ isConnected ? t('WelcomeBack') : t('welcomeTo') }}
+        <span class="text-primary">
+          {{ isConnected ? 'UserName' : 'CardClimb' }}
+        </span>
+      </h1>
+      <p
+        v-if=isConnected
+        class="text-center text-gray-500 dark:text-gray-400"
+        v-html="t('reviewMessage', { number: 7 })"
+      >
+      </p>
+      <p
+        v-else
+        class="text-center text-gray-500 dark:text-gray-400"
+      >
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim magnam esse explicabo
       </p>
     </div>
-    <div class="flex justify-around">
+    <div class="flex items-center justify-center gap-x-6 lg:gap-x-16">
       <UIcon
-        class="w-48 h-48"
+        class="w-36 h-36 lg:w-48 lg:h-48 shrink"
         name="i-heroicons-document-arrow-down"
       />
-      <div class="flex items-center">
+      <div class="flex flex-col items-center gap-y-4">
         <UButton
+          v-if="isConnected"
+          block
+          to="/review"
+          icon="i-heroicons-play-20-solid"
+        >{{ t('review') }}</UButton>
+        <UButton
+          v-if="isConnected"
+          block
+          to="/profile"
+          icon="i-heroicons-user-20-solid"
+        >{{ t('profile') }}</UButton>
+        <UButton
+          block
           to="/blitz"
-          icon="i-heroicons-bolt-solid"
-        >Blitz Mode</UButton>
+          icon="i-heroicons-bolt-20-solid"
+        >{{ t('blitz') }}</UButton>
       </div>
     </div>
   </UContainer>
