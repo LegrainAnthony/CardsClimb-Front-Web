e2ad42577a534205f12c3f11ec0fd90aee21f813
BenjaminAdrien <benadrien314@gmail.com>
2024-05-27 17:02:16 +0200

    :construction: change last column to tags

--- locales/fr-FR.ts
+++ locales/fr-FR.ts
@@ -7,6 +7,7 @@ export default defineI18nLocale(async (locale) => {
     box: 'Boîte',
     step: 'Étape',
     type: 'Type',
+    tag: 'Label',
     addBox: 'Ajouter une boîte',
     boxTitle: 'Mes boîtes de révision',
     addCard: 'Ajouter une carte',
