e9f83942e2f8c7f51652a8ac1341f76e38bd1c42
Mateo Le Fur <mateo.le-fur@hotmail.com>
2024-05-29 11:50:59 +0200

    refactor

--- composables/useCard.ts
+++ composables/useCard.ts
@@ -1,19 +1,13 @@
 const revisionCards = ref<Array<object> | null>();
 
 export const useCard = () => {
-  const getRevision = async () => {
-    const { data, pending, error, refresh } = await useCardClimbApi<
+  const getRevision = () => {
+    const { data, pending, error, status, refresh } = useCardClimbApi<
       Array<object>
-    >('http://localhost:8080/cards/user/revision-of-day', {});
-    if (!error.value) {
-      revisionCards.value = data.value;
-    }
-    return { data, pending, error, refresh };
+    >("http://localhost:8080/cards/user/revision-of-day", {});
+
+    return { data, pending, error, refresh, status };
   };
 
-  const getRevisionCardsLength = () => {
-    return revisionCards.value?.length ?? null;
-  };
-
-  return { getRevisionCardsLength, getRevision };
+  return { getRevision, revisionCards };
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,55 @@
 <script setup lang="ts">
-  definePageMeta({
-    name: "home",
-    layout: "default",
-  });
+definePageMeta({
+  name: "home",
+  layout: "default",
+});
 
-  const { t } = useI18n()
-  const toast = useToast();
+const { t } = useI18n();
+const toast = useToast();
 
-  const { user } = useMe()
+const { user } = useMe();
 
-  const { getRevision, getRevisionCardsLength } = useCard();
+const { getRevision, revisionCards } = useCard();
+const { error, status, data } = getRevision();
 
-  if (getRevisionCardsLength() === null) {
-    const { error } = await getRevision();
-    if (error.value)
-      toast.add({ title: `${error.value.statusCode} - ${error.value.statusMessage}`, color: 'red' })
+watch(status, (value) => {
+  if (value === "error" && error.value) {
+    toast.add({
+      title: `${error.value.statusCode} - ${error.value.statusMessage}`,
+      color: "red",
+    });
   }
 
-  const disableRevision = computed(() => {
-    return getRevisionCardsLength() === 0 || getRevisionCardsLength() === null
-  })
+  if (value === "success") {
+    revisionCards.value = data.value;
+  }
+});
+
+const disableRevision = computed(() => {
+  return revisionCards.value?.length === 0;
+});
 </script>
 
 <template>
   <div>
-    <UContainer class="min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center gap-y-10">
+    <UContainer
+      class="min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center gap-y-10"
+    >
       <div class="-mt-20 flex flex-col justify-center items-center gap-y-6">
         <h1 class="text-center font-bold text-4xl sm:text-5xl">
-          {{ user ? t('welcomeBack') : t('welcomeTo') }}
+          {{ user ? t("welcomeBack") : t("welcomeTo") }}
           <span class="text-primary">
-            {{ user ? user.username : 'CardClimb' }}
+            {{ user ? user.username : "CardClimb" }}
           </span>
         </h1>
         <p
-          v-if=user
+          v-if="user"
           class="text-lg text-center text-gray-500 dark:text-gray-400"
-          v-html="t('reviewMessage', { number: getRevisionCardsLength() })"
-        >
-        </p>
-        <p
-          v-else
-          class="text-lg text-center text-gray-500 dark:text-gray-400"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim magnam esse explicabo
+          v-html="t('reviewMessage', { number: revisionCards?.length || 0 })"
+        ></p>
+        <p v-else class="text-lg text-center text-gray-500 dark:text-gray-400">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim magnam
+          esse explicabo
         </p>
       </div>
       <div class="flex items-center justify-center gap-x-6 lg:gap-x-16">
@@ -56,18 +63,18 @@
             :disabled="disableRevision"
             :to="!disableRevision ? '/review' : ''"
             icon="i-heroicons-play-20-solid"
-          >{{ t('review') }}</UButton>
+            >{{ t("review") }}</UButton
+          >
           <UButton
             v-if="user"
             block
             to="/profile"
             icon="i-heroicons-user-20-solid"
-          >{{ t('profile') }}</UButton>
-          <UButton
-            block
-            to="/blitz"
-            icon="i-heroicons-bolt-20-solid"
-          >{{ t('blitz') }}</UButton>
+            >{{ t("profile") }}</UButton
+          >
+          <UButton block to="/blitz" icon="i-heroicons-bolt-20-solid">{{
+            t("blitz")
+          }}</UButton>
         </div>
       </div>
     </UContainer>
